1f5539713950b7c36b9a7c52a7c4448e7a0e3d71
Diana Arias Rojas <b60686@ecci.ucr.ac.cr>
2019-09-12 13:40:05 -0600

    Lab 2 Inge

---
+++ Lab2_B60686/Manual_ASP_NET_WEB/Manual_ASP_NET_WEB/ViewModels/InformacionGeneralViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Manual_ASP_NET_WEB.ViewModels
+{
+    //Modelo a utilizar en la vista de información general
+    public class InformacionGeneralViewModel
+    {
+        //Cantidad de pacientes
+        public int CantidadPacientes { get; set; }
+
+        //Cantidad de médicos
+        public int CantidadMedicos { get; set; }
+
+        //Cantidad de consultas
+        public int CantidadConsultas { get; set; }
+
+        //Cantidad de pacientes con peso mayor a 60
+        public int CantidadMayorSesenta { get; set; }
+    }
+}
